9e420f7f57775ba3c380141a6b0e19ea3b4d9ebc
Maria Eduarda Weber <mariaeduardaweber509@gmail.com>
2025-04-24 11:31:51 -0300

    keepinho controller

--- app/Http/Controllers/CalculosController.php
+++ app/Http/Controllers/CalculosController.php
@@ -6,7 +6,24 @@ use Illuminate\Http\Request;
 
 class CalculosController extends Controller
 {
-    function soma($x, $y){
-        return 'Soma: '.$x + $y;
+    function soma($x, $y)
+    {
+        return 'Soma: ' . $x + $y;
+    }
+    function subtracao($x, $y)
+    {
+        return 'Subtração: ' . $x - $y;
+    }
+    function multiplicacao($x, $y)
+    {
+        return 'Multiplicacao: ' . $x * $y;
+    }
+    function divisao($x, $y)
+    {
+        return 'Divisao: ' . $x / $y;
+    }
+    function quadrado($x)
+    {
+        return 'Quadrado: ' . $x * $x;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
+use App\Http\Controllers\CalculosController;
+use App\Http\Controllers\KeepinhoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/teste', function(){
-    return view('teste');
-});
+// Route::get('/teste', function(){
+//     return view('teste');
+// });
 
 Route::get('/teste/{valor}', function($valor){
     return "Você digitou: {$valor}";
@@ -21,4 +23,17 @@ Route::get('/teste/{valor}', function($valor){
 // });
 
 // Cálculos
-Route::
+Route::get('/calc/soma/{x}/{y}', [CalculosController::class, 'soma']);
+Route::get('/calc/subtracao/{x}/{y}', [CalculosController::class, 'subtracao']);
+Route::get('/calc/multiplicacao/{x}/{y}', [CalculosController::class, 'multiplicacao']);
+Route::get('/calc/divisao/{x}/{y}', [CalculosController::class, 'divisao']);
+
+// Criar a rota e a função na controller para o "quadrado" -> Elevar um único num ao quadrado
+
+Route::get('/calc/quadrado/{x}', [CalculosController::class, 'quadrado']);
+
+// Keepinho
+// Agrupamento de rotas
+Route::prefix('/keep')->group(function () {
+    Route::get('/', [KeepinhoController::class,'index']);
+});
