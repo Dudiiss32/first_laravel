383683e044477a78eae1daebf0fd30339ee0214d
Maria Eduarda Weber <mariaeduardaweber509@gmail.com>
2025-06-10 08:48:54 -0300

    create produtos

--- resources/views/produtos/create.blade.php
+++ resources/views/produtos/create.blade.php
@@ -12,11 +12,27 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                 
                     <form action="{{route('produtos.store')}}" method="post">
+                        @csrf
                         <div>
                             <x-input-label for="nome" value="Nome"/>
-                            <x-text-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required autofocus autocomplete="nome" />
+                            <x-text-input id="nome" class="block mt-1" type="text" name="nome" :value="old('nome')" required autofocus autocomplete="nome" />
                             <x-input-error :messages="$errors->get('nome')" class="mt-2" />
                         </div>
+
+                        <div>
+                            <x-input-label for="preco" value="preco"/>
+                            <x-text-input id="preco" class="block mt-1" type="number" name="preco" :value="old('preco')" required autofocus autocomplete="preco" />
+                            <x-input-error :messages="$errors->get('preco')" class="mt-2" />
+                        </div>
+                        <div>
+                            <x-input-label for="descricao" value="descricao"/>
+                            <x-textarea id="descricao" class="block mt-1" name="descricao" required autofocus autocomplete="descricao">{{old('descricao')}}</x-textarea>
+                            <x-input-error :messages="$errors->get('descricao')" class="mt-2" />
+                        </div>
+
+                        <x-primary-button>
+                            Gravar produto
+                        </x-primary-button>
                     </form>
         
                 </div>
